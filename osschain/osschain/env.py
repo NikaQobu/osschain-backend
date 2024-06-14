330f45ccd86242eb46178d9c0bb3aa8c98010c5b
NikaQobu <nikakobuladze7@gmail.com>
2024-06-14 19:36:00 +0400

    version 1.0

--- osschain/osschain/env.py
+++
@@ -1,5 +0,0 @@
-url = "https://rpc.ankr.com/multichain/f7c0df84b43c7f9f2c529c76efc01da4b30271a66608da4728f9830ea17d29bc"
-request_header = {
-            "accept": "application/json",
-            "content-type": "application/json"
-}
